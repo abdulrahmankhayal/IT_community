c089f97fb7335d71e4d6b2b82107a7a9914c687e
abdulrahmankhayal <50086316+abdulrahmankhayal@users.noreply.github.com>
2020-06-07 00:30:00 +0200

    Update object_detection_deployment.py

--- object_detection_deployment.py
+++ object_detection_deployment.py
@@ -1,13 +1,23 @@
-import tensorflow as tf
+
+import pathlib
 import numpy as np
 import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
 import zipfile
-import pathlib
-from PIL import Image
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+
 from IPython.display import display
 
-from object_detection.utils import visualization_utils as vis_util
 from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
 
 def load_model(model_name):
   base_url = 'http://download.tensorflow.org/models/object_detection/'
