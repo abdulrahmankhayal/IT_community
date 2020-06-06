bcf1773736be70a501e048a45d423cea2264f66c
abdulrahmankhayal <50086316+abdulrahmankhayal@users.noreply.github.com>
2020-06-06 23:49:33 +0200

    Add files via upload

---
+++ object_detection_deployment.py
@@ -0,0 +1,44 @@
+def load_model(model_name):
+  base_url = 'http://download.tensorflow.org/models/object_detection/'
+  model_file = model_name + '.tar.gz'
+  model_dir = tf.keras.utils.get_file(
+    fname=model_name, 
+    origin=base_url + model_file,
+    untar=True)
+
+  model_dir = pathlib.Path(model_dir)/"saved_model"
+
+  model = tf.saved_model.load(str(model_dir))
+  model = model.signatures['serving_default']
+
+  return model
+
+
+def run_inference_for_single_image(model, image):
+  image = np.asarray(image)
+  input_tensor = tf.convert_to_tensor(image)
+  input_tensor = input_tensor[tf.newaxis,...]
+
+  output_dict = model(input_tensor)
+  num_detections = int(output_dict.pop('num_detections'))
+  output_dict = {key:value[0, :num_detections].numpy() 
+                 for key,value in output_dict.items()}
+  output_dict['num_detections'] = num_detections
+  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+  return output_dict
+
+
+def show_inference(model, image_path):
+  image_np = np.array(Image.open(image_path))
+  output_dict = run_inference_for_single_image(model, image_np)
+
+  vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      use_normalized_coordinates=True,
+      line_thickness=8)
+
+  display(Image.fromarray(image_np))
